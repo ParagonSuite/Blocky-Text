7a25db7c2f999c6cf45adde260a884e27f287284
Xty <114356098+Xty21223@users.noreply.github.com>
2025-11-29 20:23:21 -0800

    Enhance error handling

--- blocky_text.py
+++ blocky_text.py
@@ -1,21 +1,34 @@
 from turtle import *
+import time as clock
 cat_shape = [(0, 30),(10, 40),(15, 30),(30, 25),(30, -10),(15, -30),(0, -25),(-15, -30),(-30, -10),(-30, 25),(-15, 30),(-10, 40),(0, 30)]
-screen = turtle.Screen()
+screen = Screen()
 screen.setup(width=1.0, height=1.0)
 screen.bgcolor("white")
 register_shape("cat", cat_shape)
 cat = Turtle()
 cat.shape("cat")
 cat.color("orange", "white")
-cat.speed(8)
+cat.speed(5)
+cat.penup()
 def moveforward(amount, object):
   try:
     object.fd(amount*2)
   except:
     print(f"Uh oh! Your code can't be read. Make sure you wrote it right.")
-
 def glideto(coordinates, object):
   try:
     object.goto(coordinates)
-  except ValueError as e:
+  except:
     print(f"Uh oh! Your code can't be read. Make sure you wrote it right.\nHere are some solutions:\n- Check your object name\n- Check the coordinates. (They should look like this: (a, b) )")
+def say(string, object):
+  try:
+    object.write(string, font=("Arial", 12, "normal"))
+  except:
+    print(f"Uh oh! Your code can't be read. Make sure you wrote it right.")
+def wait(delay):
+  try:
+    clock.sleep(delay)
+  except TypeError:
+        print(f"Error: The delay value '{delay}' is invalid. Check that it's a number!")
+  except:
+    print(f"Uh oh! Your code can't be read. Make sure you wrote it right.")
